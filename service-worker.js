eb0505f85433059e761cbf2023795bcc36e1aac1
bdv2026 <rodrigoarqbeltrao@gmail.com>
2026-07-22 17:18:56 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -32,9 +32,36 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME)
 
       .then(cache => cache.addAll(urlsToCache))
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png",
+const CACHE_NAME = "bdv-v2";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./index-mobile.html",
+  "./index-desktop.html",
+
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+
+  "./fachada.png",
+  "./tijolo-fundo.png",
+
+  "./decora.webp",
+  "./eletrodomestico.jpg",
+  "./eletroeletronico.jpg",
+  "./ferramentas.webp",
+  "./jardinagem.jpg",
+  "./suplemento.jpg",
+  "./roupa-acessorio.jpg",
+
+  "./logomarca-mercadolivre.png",
+  "./logomarca-magalu.png",
+  "./logomarca-amazon.jpg",
+  "./logomarca-Shopee.jpg",
+  "./logomarca-shein.png",
+  "./logomarca-jequiti.png"
+];
   );
 
 });
